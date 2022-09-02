dc3a7906dac1d2f82a209d989ac27e28d4945e6c
gOFFa1xexe <lol228gofa@gmail.com>
2022-09-02 13:57:51 +0300

    UPD: print in the file UPD2: exception added

--- src/Lab2RopnitskyiKI33.java
+++ src/Lab2RopnitskyiKI33.java
@@ -8,38 +8,46 @@ public class Lab2RopnitskyiKI33 {
         Scanner in = new Scanner(System.in);
         File dataFile = new File("Lab2.txt");
         PrintWriter fout = new PrintWriter(dataFile);
-       System.out.print("Please enter the array size: ");
+
+        System.out.print("Please enter the array size: ");
         arrSize = in.nextInt();
         in.nextLine();
-      System.out.print("Please enter the array filler: ");
+        System.out.print("Please enter the array filler: ");
         filler = in.nextLine();
 
+        //filling the array
         char[][] arr = new char[arrSize][];
-        int j=1;
-            for(int i =0;i<arrSize;i++) {
-                if (i < arrSize / 2) {
-                    arr[i] = new char[0];
-                } else {
-                    arr[i]=new char[(i+(++j)-arrSize/2)];
-                }
-
+        int j = 1;
+        for (int i = 0; i < arrSize; i++) {
+            if (i < arrSize / 2) {
+                arr[i] = new char[0];
+            } else {
+                arr[i] = new char[(i + (++j) - arrSize / 2)];
             }
 
-        for (int i = 0 ;i < arrSize; i++) {
-            for (int l = arrSize - i; l > 0; l--) {
-                fout.print("  ");
-                System.out.print("  ");
-            }
-            for (int y = 0; y < arr[i].length; y++) {
-                 arr[i][y] = (char) filler.codePointAt(0);
-                System.out.print(arr[i][y] + " ");
-                fout.print(arr[i][y]+ " ");
-            }
-            System.out.print("\n");
-            fout.print("\n");
         }
-        fout.flush();
-        fout.close();
-    }
 
+
+        //printing array
+        if (filler.length() == 1) {
+            for (int i = 0; i < arrSize; i++) {
+                for (int l = arrSize - i; l > 0; l--) {
+                    fout.print("  ");
+                    System.out.print("  ");
+                }
+                for (int y = 0; y < arr[i].length; y++) {
+                    arr[i][y] = filler.charAt(0);
+                    System.out.print(arr[i][y] + " ");
+                    fout.print(arr[i][y] + " ");
+                }
+                System.out.print("\n");
+                fout.print("\n");
+            }
+            fout.flush();
+            fout.close();
+        } else {
+            System.out.println("Too many characters/Filler character has not been entered");
+            
+        }
+    }
 }
